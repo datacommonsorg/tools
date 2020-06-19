414dc265d66bafa34f60c61b3e31e2fc68afbd8b
Eduardo Morales <edumorales@google.com>
2020-06-19 18:48:24 -0400

    adds calculator unit tests

--- covid19-dashboard/dc-covid19/src/DataCalculator.ts
+++ covid19-dashboard/dc-covid19/src/DataCalculator.ts
@@ -1,12 +1,9 @@
 /**
  Copyright 2020 Google LLC
-
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
  https://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +12,7 @@
  */
 
 import {addOrSubtractNDaysToDate, getRealISODatesFromArrayOfDeltaDays} from './Utils'
+import moment from "moment";
 
 type DataPerGeoIdPerDate = {string: {string: number}} | {}
 
@@ -24,20 +22,36 @@ type DataPerGeoIdPerDate = {string: {string: number}} | {}
  * @param range: the range of dates to calculate the data for in ISO Format. Example: ["2020-01-01", "2020-01-10"]
  * @param deltaDays: Do we wanna compare the data every 2 days? every 7? Any number works.
  * @param calculationType: What type of calculation are we performing?
- * @param geoIdToPopulation: geoId->Population. After calculating the difference, we want to divide by total population
+ * @param geoIdToPopulation?: geoId->Population. After calculating the difference, we want to divide by total population
  */
 export default function dataCalculator(data: DataPerGeoIdPerDate,
                                        range: [string, string],
                                        deltaDays: number,
                                        calculationType: string,
-                                       geoIdToPopulation: {geoId: number}
-                                       ): DataPerGeoIdPerDate {
-    // If there isn't any data, just return an empty {}
-    if (!data) return {}
+                                       geoIdToPopulation?: {[geoId: string]: number }
+): DataPerGeoIdPerDate {
+    // If there isn't any data, return an empty {}
+    if (!Object.keys(data).length) {
+        console.log("No input data.")
+        return {}
+    }
 
-    // If there is data,
+    const date0IsValid = moment(range[0]).isValid();
+    const date1IsValid = moment(range[1]).isValid();
+
+    // If the dates are invalid, return an empty {}
+    if (!date0IsValid || !date1IsValid) {
+        console.log("Invalid ISO dates.")
+        return {}
+    }
+    // If the ranges aren't in order, return {
+    if (moment(range[0]) >= moment(range[1])) {
+        console.log("Invalid range.")
+        return {}
+    }
+
+    // If there is data, begin calculation
     let outputData: DataPerGeoIdPerDate = {}
-
     let iterativeDate = range[0]
     const lastDayInRange = range[1]
 
@@ -54,7 +68,8 @@ export default function dataCalculator(data: DataPerGeoIdPerDate,
             const iterativeDateValue = data[iterativeDate][geoId]
             const deltaDaysFromIterativeDateValue = data[deltaDaysFromIterativeDate][geoId]
             // If one of the values is invalid, or falsy, continue.
-            if (!iterativeDateValue || !deltaDaysFromIterativeDateValue) continue
+            if (iterativeDateValue === undefined || iterativeDateValue === null) continue
+            if (deltaDaysFromIterativeDateValue === undefined || deltaDaysFromIterativeDateValue === null) continue
             // Do some calculation on the data using the calculation function.
             let result: number | null = null;
 
@@ -66,18 +81,22 @@ export default function dataCalculator(data: DataPerGeoIdPerDate,
                     result = iterativeDateValue - deltaDaysFromIterativeDateValue
                     break;
                 case 'perCapita':
-                    result = ((iterativeDateValue - deltaDaysFromIterativeDateValue) / geoIdToPopulation[geoId]) * 10000
+                    // Make sure the population is above 0, otherwise continue
+                    if (geoIdToPopulation?.[geoId] && geoIdToPopulation?.[geoId] > 0)
+                        result = ((iterativeDateValue - deltaDaysFromIterativeDateValue) / geoIdToPopulation[geoId])
                     break;
                 case 'increase':
                     result = (iterativeDateValue / deltaDaysFromIterativeDateValue) - 1
                     break;
                 case 'absolutePerCapita':
-                    result = (iterativeDateValue / geoIdToPopulation[geoId]) * 10000
+                    // Make sure the population is above 0, otherwise continue
+                    if (geoIdToPopulation?.[geoId] && geoIdToPopulation?.[geoId] > 0)
+                        result = (iterativeDateValue / geoIdToPopulation[geoId])
                     break;
             }
 
             // If the result is valid, store it
-            if (result) {
+            if (result || result === 0) {
                 // If this is the first time storing this date in the output, make some room for it.
                 if (!(iterativeDate in outputData)) outputData[iterativeDate] = {}
                 outputData[iterativeDate][geoId] = result
