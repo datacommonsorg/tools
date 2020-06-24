b42872afd2042df929cfee217f90f1cdad190ffe
Eduardo Morales <edumorales@google.com>
2020-06-24 04:36:04 -0400

    Graphs are now grouped by deltaDates. Instead of creating each row individually, we generate each Section

--- covid19-dashboard/dc-covid19/src/App.tsx
+++ covid19-dashboard/dc-covid19/src/App.tsx
@@ -14,14 +14,10 @@
  limitations under the License.
  */
 
-import React from 'react';
-import ContentFile from './ContentFile.json'
+import React from 'react'
 import Configuration from './Configuration.json'
-import {filterGeoIdThatBelongTo, filterJSONByArrayOfKeys} from './Utils'
-import {getRealISODatesFromArrayOfDeltaDays} from "./Utils";
-import SideNav from "./SideNav";
-import Row from "./Row";
-import OptionPanel from './OptionPanel'
+import {getRealISODatesFromArrayOfDeltaDays} from "./Utils"
+import Content from './Content'
 
 type State = {
     allData: {}[], // [cases, deaths, population, names]
@@ -29,7 +25,7 @@ type State = {
     selectedShowTopN: number, // default number of top counties/states to show
     datePicked: string, // current date selected, latest is always default
     availableRegions: {}, // region -> geoId
-    datesToPick: string[], // available dates to pick from, example: today, 1 week ago, 1 month ago
+    datesToPick: string[] // available dates to pick from, example: today, 1 week ago, 1 month ago,
 }
 
 class App extends React.Component <{}, State> {
@@ -42,13 +38,11 @@ class App extends React.Component <{}, State> {
         datesToPick: ["2020-01-01"]
     }
 
-    refs_: any = {}
-
     componentDidMount() {
-        this.refs_ = this.generateReferences()
         const requests = this.fetchData()
 
         Promise.all(requests).then(responses => {
+            console.log(responses)
             responses.forEach((response, index) => {
                 response.json().then( json => {
                     // Keep the data stored in the same position, that is: [cases, deaths, population, names]
@@ -72,8 +66,9 @@ class App extends React.Component <{}, State> {
                     // Index 3 contains the 'geoId -> belongsToRegion' object
                     // We can get all the available States
                     } else if (index === 3){
-                        let regions: {} = {};
+                        let regions: {} = {}
                         Object.keys(json).forEach(geoId => {
+                            // Check to see that the region belongsToRegion country/USA
                             if (json[geoId][1] === 'country/USA') regions[geoId] = json[geoId][0]
                         })
                         this.setState({availableRegions: {...this.state.availableRegions, ...regions}})
@@ -83,28 +78,6 @@ class App extends React.Component <{}, State> {
         })
     }
 
-    /**
-     * Creates a reference for each row which allows easy access from the SideNav.
-     */
-    generateReferences = () => {
-        let refs_ = {}
-        for (let key in ContentFile) refs_[key] = React.createRef<HTMLDivElement>()
-        return refs_
-    }
-
-    /**
-     * In charge of scrolling to a specific reference in the page.
-     * @param event
-     */
-    handleScrollOnRef = (event) => {
-        const ref_ = this.refs_[event.target.id]
-        if (ref_.current) {
-            ref_.current.scrollIntoView({
-                behavior: "smooth",
-                block: "nearest"
-            })
-        }
-    }
 
     /**
      * In charge of changing the state when the region, date or showTopN are updated.
@@ -115,13 +88,13 @@ class App extends React.Component <{}, State> {
         switch(key) {
             case 'region':
                 this.setState({selectedRegion: value})
-                break;
+                break
             case 'date':
                 this.setState({datePicked: value})
-                break;
+                break
             case 'showTopN':
                 this.setState({selectedShowTopN: value})
-                break;
+                break
         }
     }
 
@@ -130,70 +103,35 @@ class App extends React.Component <{}, State> {
      * The order is important [cases, deaths, population, places].
      */
     fetchData = () => {
-        const url: string = `/api/`
+        const url: string = 'http://localhost/api/'
         const apis = [
             url + 'total-cases',
             url + 'total-deaths',
             url + 'population',
-            url + 'places'];
+            url + 'places']
         // Map every url to the promise of the fetch
-        return apis.map(api => fetch(api));
-    }
-
-    /**
-     * Returns only the data corresponding to a specific region. County, State or within a State.
-     * @param allData: all the data in the form of [cases, deaths, population, names]
-     * @param region: can be State, County or a State's geoId: geoId/XX
-     */
-    getStatsDataOnlyForRegion = (allData: {}[], region: string): {}[]=> {
-        let output: {}[] = []
-        const geoIdsContainedInSelectedRegion = filterGeoIdThatBelongTo(allData[3], region)
-
-        // Iterate through all-cases and all-deaths which are found in index 0 and 1
-        for (let dataSet of this.state.allData.slice(0, 2)){
-            let filteredDataSet = {}
-            for (let date in dataSet){
-                const geoIdsForIterativeDate = dataSet[date]
-                filteredDataSet[date] = filterJSONByArrayOfKeys(geoIdsForIterativeDate, geoIdsContainedInSelectedRegion)
-            }
-            output.push(filteredDataSet)
-        }
-
-        output.push(this.state.allData[2])
-        output.push(this.state.allData[3])
-        return output
+        return apis.map(api => fetch(api))
     }
     
     render = () => {
-        const allDataForSelectedRegion = this.getStatsDataOnlyForRegion(this.state.allData, this.state.selectedRegion)
-        const rows = Object.keys(ContentFile).map(panelId => <Row allData={allDataForSelectedRegion}
-                                                                  panelId={panelId}
-                                                                  datePicked={this.state.datePicked}
-                                                                  region={this.state.selectedRegion}
-                                                                  ref_={this.refs_[panelId]}
-                                                                  selectedShowTopN={this.state.selectedShowTopN}/>)
-
         return (
             <div className={"container"}>
-                <SideNav handleScrollOnRef={this.handleScrollOnRef}/>
-                <div className={"main-content"}>
-                    <h1 className={"main-title"}>
-                        {Configuration.TITLE + ' '}
-                        <span style={{color: "#990001"}}>{Configuration.SUBTITLE}</span>
-                    </h1>
-                    <OptionPanel handleSelectUpdate={this.handleSelectUpdate}
-                                 geoIdToName={this.state.allData[3]}
-                                 defaultShowTopN={this.state.selectedShowTopN}
-                                 datesToPick={this.state.datesToPick}
-                                 datePicked={this.state.datePicked}
-                                 availableRegions={this.state.availableRegions}
-                                 selectedRegion={this.state.selectedRegion}/>
-                        {rows}
-                </div>
-                <h5 className={"footer"}>{Configuration.FOOTER}</h5>
+                <h1 className={"main-title"}>
+                    {Configuration.TITLE + ' '}
+                    <span style={{color: "#990001"}}>{Configuration.SUBTITLE}</span>
+                </h1>
+                <Content allData={this.state.allData}
+                         content={Configuration.content}
+                         selectedRegion={this.state.selectedRegion}
+                         availableRegions={this.state.availableRegions}
+                         datePicked={this.state.datePicked}
+                         datesToPick={this.state.datesToPick}
+                         region={this.state.selectedRegion}
+                         handleSelectUpdate={this.handleSelectUpdate}
+                         selectedShowTopN={this.state.selectedShowTopN}/>
             </div>
         )
     }
 }
 
-export default App;
+export default App
