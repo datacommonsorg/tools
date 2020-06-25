b3f846372da6fe2322974074d1da8b995901c6e4
Eduardo Morales <edumorales@google.com>
2020-06-24 21:40:23 -0400

    lines are now 80 characters

--- covid19-dashboard/dc-covid19/src/MetaDataGenerator.ts
+++ covid19-dashboard/dc-covid19/src/MetaDataGenerator.ts
@@ -12,9 +12,9 @@
  */
 
 /**
- * Given two values (or one if absolute or absolutePerCapita), return the result of calculationType.
- * @param values: [number, number] -> values[0] is date - deltaDays, values[1] is the actual date.
- * @param calculationType: the type of calculating we are performing on the values
+ * Given two values, return the result of calculationType.
+ * @param values: values[0] is date - deltaDays, values[1] is the date.
+ * @param calculationType: the type of calculating we are performing
  */
 
 import {numberWithCommas} from "./Utils";
@@ -22,112 +22,150 @@ import {numberWithCommas} from "./Utils";
 type DateToGeoIdToValue = {[geoId: string]: {[date: string]: number}} | {}
 
 /**
- * Given a list of parameters, return human readable strings explaining the data.
+ * Returns strings briefly explaining the calculation performed
  * @param geoId: the geoId of the region
  * @param regionName: the name of the region
  * @param date: the date of the observation
  * @param value: the value at that specific date for that specific geoId
  * @param population: the total population of that region
- * @param absoluteIncreaseValue: the absolute difference between day 2 and day 1
+ * @param absolute: the absolute difference between day 2 and day 1
  * @param label: Are we talking about cases? or deaths?
- * @param calculationType: What is the graph showing? perCapita? Increase? Absolute?
+ * @param calculationType: calculations performed on data: eg. ['difference']
+ * @return onHoverInfo: human readable text to display on graph's on-hover
  */
-const getOnHoverInfoText = (geoId: string, regionName: string, date: string, value: number, population: number,
-                            absoluteIncreaseValue: number, label: string, calculationType: string[]) => {
-    let disclaimer: string = ""
-    let onHoverInfo: string[] = [`${numberWithCommas(value)} ${label}`]
+const getOnHoverInfoText = (geoId: string,
+                            regionName: string,
+                            date: string,
+                            value: number,
+                            population: number,
+                            absolute: number,
+                            label: string,
+                            calculationType: string[]) => {
+    let disclaimer = ""
+    let onHoverInfo = [`${numberWithCommas(value)} ${label}`]
 
-    // If it's New York City or Kansas City. Show disclaimer. These are NYT exceptions.
-    if (['geoId/3651000', 'geoId/2938000'].includes(geoId))
-        disclaimer = `All counties in ${regionName} are combined and reported as one.`
-
-    if (calculationType.includes('perCapita')) {
-        onHoverInfo = [
-            `${(value * 10000).toFixed(3)} ${label} per 10,000 people`,
-            `New ${label}: ${numberWithCommas(absoluteIncreaseValue)}`,
-            `Total population: ${numberWithCommas(population)} people`]
-    } else if (calculationType.includes('increase')) {
-        onHoverInfo = [
-            `Percent increase: ${numberWithCommas(Math.round(value * 100))}%`,
-            `Absolute increase: ${numberWithCommas(absoluteIncreaseValue)} ${label}`]
+    // If it's New York City or Kansas City. Show a disclaimer.
+    // These are NYT exceptions.
+    if (['geoId/3651000', 'geoId/2938000'].includes(geoId)) {
+        disclaimer = `All counties in ${regionName}` +
+            'are combined and reported as one.'
     }
 
-    if (disclaimer) return [disclaimer, ...onHoverInfo]
-    else return onHoverInfo
+    if (calculationType.includes('perCapita')) {
+        const per10000People = (value * 10000).toFixed(3)
+        onHoverInfo = [
+            ` ${per10000People }${label} per 10,000 people`,
+            `New ${label}: ${numberWithCommas(absolute)}`,
+            `Total population: ${numberWithCommas(population)} people`
+        ]
+    } else if (calculationType.includes('increase')) {
+        const percentIncrease = numberWithCommas(Math.round(value * 100))
+        const absoluteWithCommas = numberWithCommas(absolute)
+        onHoverInfo = [
+            `Percent increase: ${percentIncrease}%`,
+            `Absolute increase: ${absoluteWithCommas} ${label}`
+        ]
+    }
+
+    if (disclaimer) {
+        return [disclaimer, ...onHoverInfo]
+    } else {
+        return onHoverInfo
+    }
 }
 
 /**
- * Given a list of parameters, return human readable value that will be displayed on top of the graph's bar.
+ * Returns strings display on top of graph's bar.
  * @param geoId: the geoId of the region
  * @param regionName: the name of the region
  * @param date: the date of the observation
  * @param value: the value at that specific date for that specific geoId
  * @param population: the total population of that region
- * @param absoluteIncreaseValue: the absolute difference between day 2 and day 1
+ * @param absoluteIncrease: the absolute difference between day 2 and day 1
  * @param label: Are we talking about cases? or deaths?
- * @param calculationType: What is the graph showing? Per-Capita? Increase? Absolute?
+ * @param calculationType: calculations performed on data: eg. ['difference']
+ * @return onHoverInfo: human readable text on top of
  */
-const getTextOnTopOfBar = (geoId: string, regionName: string, date: string, value: number, population: number,
-                           absoluteIncreaseValue: number, label: string, calculationType: string[]) => {
-    let textOnTopOfBar: string = `${numberWithCommas(value)}`
+const getTextOnTopOfBar = (geoId: string,
+                           regionName: string,
+                           date: string,
+                           value: number,
+                           population: number,
+                           absoluteIncrease: number,
+                           label: string,
+                           calculationType: string[]): string => {
 
-    if (calculationType.includes('perCapita'))
-        textOnTopOfBar = `${numberWithCommas(absoluteIncreaseValue)} / ${numberWithCommas(population)}`
-    else if (calculationType.includes('increase'))
-        textOnTopOfBar = `${numberWithCommas(absoluteIncreaseValue)} (${Math.round(value * 100)}%)`
+    const absoluteWithCommas = numberWithCommas(absoluteIncrease)
 
-    return textOnTopOfBar
+    if (calculationType.includes('perCapita')) {
+        const populationCommas = numberWithCommas(population)
+        return `${absoluteWithCommas} / ${populationCommas}`
+    } else if (calculationType.includes('increase')) {
+        const increasePerCent = Math.round(value * 100)
+        return `${absoluteWithCommas} (${increasePerCent}%)`
+    } else {
+        return `${numberWithCommas(value)}`
+    }
 }
 
 /**
- * Generates information for the graph to display. For example, the Bar Graph must know things such as 'name',
+ * Generates an object for the information to display.
+ * For example, the Bar Graph must know things such as 'value', 'name',
  * 'onHoverInfo', and the 'text' to display on top of the bar.
  * @param dateToGeoIdToValue: date->geoId->value
  * @param geoIdToPopulation: geoId->Population
  * @param geoIdToName: geoId->name
- * @param dateToGeoIdToAbsolute: absolute number from date X to date Y (optional)
+ * @param dateToGeoIdToAbsolute: absolute number from date X to date Y
  * @param label: Are we talking about cases or deaths?
- * @param calculationType: What is the graph showing? Per-Capita? Increase? Absolute?
+ * @param calculationType: calculations performed on data: eg. ['difference']
  */
-export default function generateMetadata(dateToGeoIdToValue: DateToGeoIdToValue,
-                                         geoIdToPopulation: {[geoId: string]: number},
-                                         geoIdToName: {[geoId: string]: string},
-                                         dateToGeoIdToAbsolute: DateToGeoIdToValue,
-                                         label: string,
-                                         calculationType: string[]): DateToGeoIdToValue {
+function generateMetadata(dateToGeoIdToValue: DateToGeoIdToValue,
+                          geoIdToPopulation: {[geoId: string]: number},
+                          geoIdToName: {[geoId: string]: string},
+                          dateToGeoIdToAbsolute: DateToGeoIdToValue,
+                          label: string,
+                          calculationType: string[]): DateToGeoIdToValue {
+
     let output: DateToGeoIdToValue | {} = {}
 
     // For every date and geoId in the dataset
     for (let date in dateToGeoIdToValue){
         for (let geoId in dateToGeoIdToValue[date]) {
-            const iterativePopulation: number = geoIdToPopulation[geoId] || 0
+            const iterativePopulation = geoIdToPopulation[geoId]
             // Get the name of the state, and get rid of county or Parish
-            let iterativeRegionName: string = geoIdToName[geoId]?.[0] || geoId
-            iterativeRegionName.replace(" County", "")
-                               .replace(" Parish", "")
-                               .replace(" Borough", "")
-            // iterativeBelongingRegion holds the region that it belongs to, for example geoId/12345 belongs to geoId/12
-            const iterativeBelongingRegion: string = geoIdToName[geoId]?.[1]
-            const iterativeValue: number = dateToGeoIdToValue[date]?.[geoId] || 0
-            const iterativeAbsoluteIncreaseValue: number = dateToGeoIdToAbsolute[date]?.[geoId] || 0
+            let regionName: string = geoIdToName[geoId]?.[0] || geoId
 
-            // If the region not a State, append the State to the name. Example: "Miami, Florida"
-            if (iterativeBelongingRegion.includes("geoId"))
-                iterativeRegionName += ", " + geoIdToName[iterativeBelongingRegion]?.[0]
+            regionName.replace(" County", "")
+                .replace(" Parish", "")
+                .replace(" Borough", "")
+
+            // containedIn holds the region that it belongs to
+            // For example geoId/12345 belongs to geoId/12
+            const containedIn: string = geoIdToName[geoId]?.[1]
+            const value: number = dateToGeoIdToValue[date]?.[geoId]
+            const absolute: number = dateToGeoIdToAbsolute[date]?.[geoId]
+
+            // If the region is not a State, append the State to the name.
+            // Example: "Miami, Florida"
+            if (containedIn.includes("geoId")) {
+                regionName += ", " + geoIdToName[containedIn]?.[0]
+            }
 
             // Generate the text that is displayed when the user hovers on a graph.
-            const onHoverInfo = getOnHoverInfoText(geoId, iterativeRegionName, date, iterativeValue,
-                iterativePopulation, iterativeAbsoluteIncreaseValue, label, calculationType)
+            const onHoverInfo = getOnHoverInfoText(geoId, regionName, date, value,
+                iterativePopulation, absolute, label, calculationType)
 
-            const textOnTopOfBar = getTextOnTopOfBar(geoId, iterativeRegionName, date, iterativeValue,
-                iterativePopulation, iterativeAbsoluteIncreaseValue, label, calculationType)
+            const textOnTopOfBar = getTextOnTopOfBar(geoId, regionName, date, value,
+                iterativePopulation, absolute, label, calculationType)
 
             // If the date is not in the output, create it.
-            if (!(date in output)) output[date] = {}
+            if (!(date in output)){
+                output[date] = {}
+            }
 
             output[date][geoId] = {
                 geoId: geoId,
-                name: iterativeRegionName,
+                name: regionName,
                 onHoverInfo: onHoverInfo,
                 textOnTopOfBar: textOnTopOfBar,
                 value: dateToGeoIdToValue[date][geoId]
@@ -136,3 +174,5 @@ export default function generateMetadata(dateToGeoIdToValue: DateToGeoIdToValue,
     }
     return output
 }
+
+export default generateMetadata
