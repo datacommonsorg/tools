7d42feb81e2ad0af7dfa1b683cf0878269c22a7e
Eduardo Morales <edumorales@google.com>
2020-06-29 20:50:22 -0400

    reformat Util.ts and Panel.tsx (#17)
    * reformat util and panel
    
    * function name changed
    
    * get rid of one line if statements
    
    * Update indent for Panel.tsx
    
    * Upate indentation Utils.js

--- covid19-dashboard/dc-covid19/src/Utils.ts
+++ covid19-dashboard/dc-covid19/src/Utils.ts
@@ -14,21 +14,27 @@
  limitations under the License.
  */
 
-import moment from "moment";
-
+import moment from 'moment';
 
 /**
  * Given a date and an array of deltaDays compute the real ISODates.
- * For example, date = 2020-01-10 and deltaDays = [0, 1, 7] will return an array with the ISODates
- * [2020-01-10, 2020-01-09, 2020-01-03] because 0 represents today, 1 represents 1 day ago, and 7 represents 7 days ago.
- * @param date
- * @param deltaDays
+ * For example, date = 2020-01-10 and deltaDays = [0, 1, 7]
+ * will return the following: [2020-01-10, 2020-01-09, 2020-01-03]
+ * 0 represents today, N represents N days ago.
+ * @param date: the date we are observing
+ * @param deltaDays: the date
  */
-const getRealISODatesFromArrayOfDeltaDays = (date: string, deltaDays: number[]): string[] => {
-    if (date === undefined || date === null) return []
-    const dates = deltaDays.map(deltaDate => addOrSubtractNDaysToDate(date, -deltaDate))
-    return dates.sort((a, b) => (a < b) ? -1 : ((a > b) ? 1 : 0));
-}
+const getRealISODatesFromArrayOfDeltaDays = (
+  date: string,
+  deltaDays: number[]
+): string[] => {
+  if (!date) {
+    return [];
+  }
+
+  const dates = deltaDays.map(deltaDate => addNDaysToDate(date, -deltaDate));
+  return dates.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+};
 
 /**
  * Converts a number to a string including commas for readability.
@@ -36,21 +42,12 @@ const getRealISODatesFromArrayOfDeltaDays = (date: string, deltaDays: number[]):
  * @param num: the number to replace
  */
 const numberWithCommas = (num: number): string => {
-    if (!num) return "0"
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-}
+  if (!num) {
+    return '0';
+  }
 
-/**
- * Converts an ISO date to English date.
- * For example, 2020-01-01 is converted to January 1st, 2020.
- * @param ISOdate: a string representing an ISO date.
- */
-const prettifyDate = (ISOdate: string): string => {
-    const prettifiedDate = moment(ISOdate).format('MMMM Do, YYYY')
-    // If prettifiedDate is not valid, it means it wasn't a ISOdate input.
-    if (!prettifiedDate) return "Some Day"
-    return prettifiedDate
-}
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 
 /**
  * Adds or subtracts any number of days to an ISO date.
@@ -59,14 +56,18 @@ const prettifyDate = (ISOdate: string): string => {
  * @param ISOdate: a string representing a date in ISO format
  * @param days: either positive or negative days to add
  */
-const addOrSubtractNDaysToDate = (ISOdate: string, days: number): string => {
-    let date;
-    // Subtracting to date
-    if (days < 0) date = moment(ISOdate).subtract(Math.abs(days), "days")
-    // Adding to date
-    else date = moment(ISOdate).add(Math.abs(days), "days")
-    return date.format("YYYY-MM-DD")
-}
+const addNDaysToDate = (ISOdate: string, days: number): string => {
+  let date;
+
+  // Negative dates passed in -- subtracting
+  if (days < 0) {
+    date = moment(ISOdate).subtract(Math.abs(days), 'days');
+    // Positive date passed in -- adding
+  } else {
+    date = moment(ISOdate).add(Math.abs(days), 'days');
+  }
+  return date.format('YYYY-MM-DD');
+};
 
 /**
  * Returns an array containing [initialDate, lastDate]
@@ -75,40 +76,60 @@ const addOrSubtractNDaysToDate = (ISOdate: string, days: number): string => {
  * @param ISOdate: date to look at
  * @param deltaDays: number of days before our ISOdate to look at
  */
-const getRangeOfDates = (ISOdate: string, deltaDays: number): [string, string] => {
-    // [ISOdate - deltaDays, ISOdate]
-    return [addOrSubtractNDaysToDate(ISOdate, -deltaDays), ISOdate]
-}
+const getRangeOfDates = (
+  ISOdate: string,
+  deltaDays: number
+): [string, string] => {
+  // Will return [ISOdate - deltaDays, ISOdate]
+  return [addNDaysToDate(ISOdate, -deltaDays), ISOdate];
+};
 
 /**
- * Given a map of geoId->[name, belongToRegion], it returns a list of the geoId's that belong to the belongToRegion
- * @param geoIdToType: geoId->[name, belongToRegion]
- * @param belongToRegion: can be any geoId
+ * Given a map of geoId->[name, belongsToRegion]
+ * Returns a list of the geoId's that belong to a region
+ * @param geoIdToInfo: geoId->[name, belongsToRegion]
+ * @param belongsToRegion: can be any geoId
  */
-const filterGeoIdThatBelongTo = (geoIdToType: {geoId: string[]} | {}, belongToRegion: string): string[] => {
-    if (belongToRegion === 'County') return Object.keys(geoIdToType).filter(geoId => geoIdToType[geoId][1] !== 'country/USA')
-    else if (belongToRegion === 'State') return Object.keys(geoIdToType).filter(geoId => geoIdToType[geoId][1] === 'country/USA')
-    else return Object.keys(geoIdToType).filter(geoId => geoIdToType[geoId][1] === belongToRegion)
-}
+const filterByRegionsContainedIn = (
+  geoIdToInfo: {geoId: string[]} | {},
+  belongsToRegion: string
+): string[] => {
+  if (belongsToRegion === 'County') {
+    return Object.keys(geoIdToInfo).filter(
+      geoId => geoIdToInfo[geoId][1] !== 'country/USA'
+    );
+  } else if (belongsToRegion === 'State') {
+    return Object.keys(geoIdToInfo).filter(
+      geoId => geoIdToInfo[geoId][1] === 'country/USA'
+    );
+  } else {
+    return Object.keys(geoIdToInfo).filter(
+      geoId => geoIdToInfo[geoId][1] === belongsToRegion
+    );
+  }
+};
 
 /**
- * Returns new copy of the inputted JSON with only the keys that are present in the array.
+ * Returns new copy of the inputted JSON only containing keys present.
  * This is similar to the reduce() function, but for JSON.
  * @param JSON: any object
  * @param keys: an array of keys
  */
-const filterJSONByArrayOfKeys = (JSON: {}, keys: any[], ): {} => {
-    let output: {} = {}
-    keys.forEach(key => {if (key in JSON) output[key] = JSON[key]})
-    return output
-}
+const filterJSONByArrayOfKeys = (JSON: {}, keys: string[]): {} => {
+  const output = {};
+  keys.forEach(key => {
+    if (key in JSON) {
+      output[key] = JSON[key];
+    }
+  });
+  return output;
+};
 
 export {
-    getRealISODatesFromArrayOfDeltaDays,
-    numberWithCommas,
-    prettifyDate,
-    addOrSubtractNDaysToDate,
-    getRangeOfDates,
-    filterGeoIdThatBelongTo,
-    filterJSONByArrayOfKeys
-}
+  getRealISODatesFromArrayOfDeltaDays,
+  numberWithCommas,
+  addNDaysToDate,
+  getRangeOfDates,
+  filterByRegionsContainedIn,
+  filterJSONByArrayOfKeys,
+};
