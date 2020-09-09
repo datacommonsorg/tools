16c3ff2cf46337241cb5b5f5c6e9227e8ba2bd7c
Christie Lincoln <clincoln@ucsd.edu>
2020-09-09 14:25:10 -0700

    fixed import statement of parse-mcf.js

--- mcf-browser/src/back-end/parse-tmcf.js
+++ mcf-browser/src/back-end/parse-tmcf.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {shouldReadLine} from './ParseMCF.js';
+import {shouldReadLine} from './parse-mcf.js';
 
 const csv = require('csvtojson');
 
