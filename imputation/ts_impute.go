c4cd35a542c047b04d43d80318df791cfae7ea3e
eftekhari-mhs <eftekhari.mhs@gmail.com>
2020-09-14 15:00:26 +0000

    n Median imputation and fillNA with zero (#43)
    FillMean is changed to FillNA(TimeSeries, method)
    Implemented methods: mean, zero, median

--- imputation/ts_impute.go
+++ imputation/ts_impute.go
@@ -20,27 +20,43 @@ import (
 // TimeSeries stores a time series.  Dates are expressed in ISO 8061 format as illustrated in the constants below.
 type TimeSeries map[string]float64
 
-// Assume 3 formattig for the dates: daily, monthly, and yearly.
+// Assume 3 formatting for the dates: daily, monthly, and yearly.
 const (
 	dayfmt   = "2006-01-02"
 	monthfmt = "2006-01"
 	yearfmt  = "2006"
 )
 
-// The diff function returns the minimum gap (will change to GCD) from a given set of data points
-// that all have the same format.
+// dateGapFinder returns the minimum time gap for the given times.
+//
+// Args:
+//   keys: A sorted list of times all with the same format, one of those listed above.
+// Returns:
+//   1) The time format of the keys.
+//   2) The minimum gap between the keys in terms of years, months, and days.
+// TODO(eftekhari-mbs): Modify the algorithm to compute a greatest common divisor-like algorithm, not
+// just the minimum gap.
 // TODO(eftekhari-mhs): add OK, ERR return values and handle errors.
-func diff(keys []string, dateFormat string) (int, int, int) {
+func dateGapFinder(keys []string) (string, int, int, int) {
+	var parseFormat string
+	switch len(keys[0]) {
+	case 4:
+		parseFormat = yearfmt
+	case 7:
+		parseFormat = monthfmt
+	case 10:
+		parseFormat = dayfmt
+	}
+
 	year, month, day := 0, 0, 0
 
-	duration := 1000 //A large number.
+	duration := 1<<31 - 1 // A large number.
 
-	sort.Strings(keys)
 	for i := 0; i < len(keys)-1; i++ {
-		start, _ := time.Parse(dateFormat, keys[i])
-		end, _ := time.Parse(dateFormat, keys[i+1])
+		start, _ := time.Parse(parseFormat, keys[i])
+		end, _ := time.Parse(parseFormat, keys[i+1])
 
-		// Calculate total number of days.
+		// Calculate total number of days between each two points and compare to minimum gap so far.
 		if delta := int(end.Sub(start).Hours() / 24); duration > delta { //TODO: instead of min use GCD
 			duration = delta
 			y1, M1, d1 := start.Date()
@@ -52,51 +68,134 @@ func diff(keys []string, dateFormat string) (int, int, int) {
 		}
 	}
 
-	return year, month, day
+	return parseFormat, year, month, day
 }
 
-// FillMean returns TimeSeries with missing datapoints with value = mean(value of existing points).
+func getSortedKeys(ts TimeSeries) []string {
+	keys := make([]string, 0)
+	for k, _ := range ts {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func getValues(ts TimeSeries) []float64 {
+	values := make([]float64, 0)
+	for _, v := range ts {
+		values = append(values, v)
+	}
+	return values
+}
+
+// FillNA returns TimeSeries with missing datapoints with assigned values according to the selected method.
+// Method can be "mean", "median", and "zero".
 // TODO(eftekhari-mhs): Handle error cases.
-func FillMean(ts TimeSeries) (TimeSeries, error) {
+func FillNA(ts TimeSeries, method string) (TimeSeries, error) {
 	if len(ts) < 3 {
-		log.Printf("not enough data to impute")
+		log.Printf("There is not enough data to impute.")
 		return ts, nil
 	}
-	keys := make([]string, 0)
-	var mean float64 = 0
-	for k, v := range ts {
-		keys = append(keys, k)
-		mean += v
-	}
-	mean = mean / float64(len(keys))
+	keys := getSortedKeys(ts)
+	values := getValues(ts)
 
-	var parseFormat string
-	switch len(keys[0]) {
-	case 4:
-		parseFormat = yearfmt
-	case 7:
-		parseFormat = monthfmt
-	case 10:
-		parseFormat = dayfmt
-	}
-
+	parseFormat, yStep, mStep, dStep := dateGapFinder(keys)
 	if parseFormat == "" {
-		return ts, errors.New("date format is not ISO 8601")
+		return ts, errors.New("The date format is not ISO 8601.")
 	}
-
-	yStep, mStep, dStep := diff(keys, parseFormat)
 
 	log.Printf("Step is equal to : %v years, %v months, %v days \n", yStep, mStep, dStep)
 
-	sort.Strings(keys)
+	// Computing the filling value.
+	var fillV float64
+	switch method {
+	case "mean":
+		for _, v := range values {
+			fillV += v
+		}
+		fillV = fillV / float64(len(keys))
+	case "zero":
+		fillV = 0
+	case "median":
+		sort.Float64s(values)
+		if len(values)%2 == 0 {
+			// In case of even length: median = average of two middle values.
+			fillV = (values[len(values)/2] + values[len(values)/2-1]) / 2
+			log.Printf("Median is %v: for even case", fillV)
+		} else {
+			fillV = values[len(values)/2]
+			log.Printf("Median is %v: for odd case", fillV)
+		}
+	default:
+		return ts, errors.New("The method is unknown.")
+	}
 
-	//TODO(eftekhari-mhs): Handle errors.
+	// TODO(eftekhari-mhs): Handle errors.
 	startDate, _ := time.Parse(parseFormat, keys[0])
 	endDate, _ := time.Parse(parseFormat, keys[len(keys)-1])
 
 	for d := startDate; d.After(endDate) == false; d = d.AddDate(yStep, mStep, dStep) {
 		if _, ok := ts[fmt.Sprint(d.Format(parseFormat))]; !ok {
-			ts[fmt.Sprint(d.Format(parseFormat))] = mean
+			ts[fmt.Sprint(d.Format(parseFormat))] = fillV
+		}
+	}
+	return ts, nil
+}
+
+// Interpolate function's input is a TimeSeries with possible missing datapoints.
+// The function returns a Timeseries with no missing dates. Interpolation method is used to fill the values.
+// Methods can vary by degree: d = {1} is implemented.
+func Interpolate(ts TimeSeries, degree int) (TimeSeries, error) {
+	if len(ts) < 3+(degree-1) {
+		log.Printf("There is not enough data to impute.")
+		return ts, nil
+	}
+
+	switch degree {
+	case 1:
+		return linear(ts)
+	default:
+		return ts, errors.New("Not implemented yet.")
+	}
+}
+
+// Linear interpolation; this is eqivalent to Spline degree 1.
+func linear(ts TimeSeries) (TimeSeries, error) {
+	keys := getSortedKeys(ts)
+
+	parseFormat, yStep, mStep, dStep := dateGapFinder(keys)
+	log.Printf("Step is equal to : %v years, %v months, %v days \n", yStep, mStep, dStep)
+
+	//TODO(eftekhari-mhs): Handle errors.
+	startDate, _ := time.Parse(parseFormat, keys[0])
+	endDate, _ := time.Parse(parseFormat, keys[len(keys)-1])
+	// Assuming the time series is the form of {date1 : y1, date2: y2 ,date3 : y3} and y2
+	// is the missing value. d21 = date2-date1 and d31 = date3-date1
+	var d21, d31, y1, y2, y3 float64
+	nextDate := startDate
+
+	for d := startDate; d.After(endDate) == false; d = d.AddDate(yStep, mStep, dStep) {
+		if _, ok := ts[fmt.Sprint(d.Format(parseFormat))]; !ok {
+			d21 += 1
+			// Looking for the next available date (date3) and y3. Store gap in d31.
+			if nextDate.Before(d) {
+				d31 = d21
+				nextDate = d
+				for !ok {
+					nextDate = nextDate.AddDate(yStep, mStep, dStep)
+					_, ok = ts[fmt.Sprint(nextDate.Format(parseFormat))]
+					d31 += 1
+				}
+				y3, _ = ts[fmt.Sprint(nextDate.Format(parseFormat))]
+			}
+
+			y2 = ((d21) * (y3 - y1) / (d31)) + y1
+			ts[fmt.Sprint(d.Format(parseFormat))] = y2
+		} else {
+			d21 = 0
+			y1, _ = ts[fmt.Sprint(d.Format(parseFormat))]
+			// Reset next available date.
+			nextDate = startDate
 		}
 	}
 	return ts, nil
--- imputation/ts_impute_test.go
+++ imputation/ts_impute_test.go
@@ -1,8 +1,15 @@
 package imputation
 
 import (
+	"math"
+	"sort"
 	"testing"
 )
+
+// Data structure for storing precomputed values of each interpolation method.
+type InterpolateValues struct {
+	linear float64
+}
 
 // computeMeans computes the mean of the given non-empty time series's data.
 func computeMean(s TimeSeries) float64 {
@@ -14,102 +21,260 @@ func computeMean(s TimeSeries) float64 {
 	return mean
 }
 
-// createOutput generate a new time series by adding desiredValues as data points and replace
-// their value by mean.
-func createOutput(ts TimeSeries, desiredValues []string) TimeSeries {
+// computeMedian computes the median value of the given non-empty time series's data.
+func computeMedian(s TimeSeries) float64 {
+	values := make([]float64, 0)
+	for _, v := range s {
+		values = append(values, v)
+	}
+	sort.Float64s(values)
+	if len(values)%2 == 0 {
+		return (values[len(values)/2] + values[len(values)/2-1]) / 2
+	} else {
+		return values[len(values)/2]
+	}
+}
+
+// createExpectedTimeSeriesFillNA generate a new time series by adding desiredValues as data points and replace
+// their value by using the specified algorithm.
+func createExpectedTimeSeriesFillNA(ts TimeSeries, desiredValues []string, method string) TimeSeries {
 	out := make(TimeSeries, len(ts)+len(desiredValues))
 	for k, v := range ts {
 		out[k] = v
 	}
-	if len(ts) > 0 {
-		m := computeMean(ts)
+	switch method {
+	case "mean":
+		if len(ts) > 0 {
+			m := computeMean(ts)
+			for _, k := range desiredValues {
+				out[k] = m
+			}
+		}
+	case "zero":
 		for _, k := range desiredValues {
-			out[k] = m
+			out[k] = 0
+		}
+	case "median":
+		if len(ts) > 0 {
+			median := computeMedian(ts)
+			for _, k := range desiredValues {
+				out[k] = median
+			}
 		}
 	}
 	return (out)
 }
 
-func TestMean(t *testing.T) {
-	tests := map[string]struct {
-		series         TimeSeries
-		additionalKeys []string
-	}{
-		"empty": {
-			series:         TimeSeries{},
-			additionalKeys: []string{},
-		},
-		"length 1": {
-			series:         TimeSeries{"2003": 42.5},
-			additionalKeys: []string{},
-		},
-		"length 3, years": {
-			series: TimeSeries{
-				"2003": 42.5,
-				"2002": 18.9,
-				"1998": 28,
-			},
-			additionalKeys: []string{"1999", "2000", "2001"},
-		},
-		"length 3, 4-month gap": {
-			series: TimeSeries{
-				"2003-02": 42.5,
-				"2002-02": 18.9,
-				"2003-04": 28,
-			},
-			// TODO(eftekhari-mhs): edge case : 2002-01 fails currently.
-			additionalKeys: []string{"2002-04", "2002-06", "2002-08", "2002-10", "2002-12"},
-		},
-		"days": {
-			series: TimeSeries{
-				"2002-03-01": 42.5,
-				"2002-02-01": 18.9,
-				"2002-02-02": 28,
-			},
-			additionalKeys: []string{
-				"2002-02-03",
-				"2002-02-04",
-				"2002-02-05",
-				"2002-02-06",
-				"2002-02-07",
-				"2002-02-08",
-				"2002-02-09",
-				"2002-02-10",
-				"2002-02-11",
-				"2002-02-12",
-				"2002-02-13",
-				"2002-02-14",
-				"2002-02-15",
-				"2002-02-16",
-				"2002-02-17",
-				"2002-02-18",
-				"2002-02-19",
-				"2002-02-20",
-				"2002-02-21",
-				"2002-02-22",
-				"2002-02-23",
-				"2002-02-24",
-				"2002-02-25",
-				"2002-02-26",
-				"2002-02-27",
-				"2002-02-28",
-			},
-		},
+// createExpectedTimeSeriesInterpolate generate a new time series by adding desiredValues as data points and replace
+// their value using given precomputed values for each method.
+func createExpectedTimeSeriesInterpolate(ts TimeSeries, desiredValues map[string]InterpolateValues, method string) TimeSeries {
+	out := make(TimeSeries, len(ts)+len(desiredValues))
+	for k, v := range ts {
+		out[k] = v
 	}
-	for label, test := range tests {
-		want := createOutput(test.series, test.additionalKeys)
-		got, err := FillMean(test.series)
-		if err != nil {
-			t.Log(label, err)
+	switch method {
+	case "linear":
+		if len(ts) > 2 {
+			for k, v := range desiredValues {
+				out[k] = v.linear
+			}
 		}
-		if len(got) != len(want) {
-			t.Errorf("%s: Size mismatch: FillMean(%v) = %v, want %v",
-				label, test.series, got, want)
+	}
+	return (out)
+}
+
+// Test function for FillNA; possible methods = {mean, median, zero}
+func TestFillNA(t *testing.T) {
+	methods := []string{"mean", "zero", "median"}
+	for _, m := range methods {
+		tests := map[string]struct {
+			series         TimeSeries
+			additionalKeys []string
+		}{
+			"empty": {
+				series:         TimeSeries{},
+				additionalKeys: []string{},
+			},
+			"length 1": {
+				series:         TimeSeries{"2003": 42.5},
+				additionalKeys: []string{},
+			},
+			"length 3, years": {
+				series: TimeSeries{
+					"2003": 42.5,
+					"2002": 18.9,
+					"1998": 28,
+					"1996": 25,
+				},
+				additionalKeys: []string{"1999", "2000", "2001", "1997"},
+			},
+			"length 3, 2-month gap": {
+				series: TimeSeries{
+					"2003-02": 42.5,
+					"2002-02": 18.9,
+					"2003-04": 28,
+				},
+				// TODO(eftekhari-mhs): edge case : 2002-01 fails currently.
+				additionalKeys: []string{"2002-04", "2002-06", "2002-08", "2002-10", "2002-12"},
+			},
+			"days": {
+				series: TimeSeries{
+					"2002-03-01": 42.5,
+					"2002-02-01": 18.9,
+					"2002-02-02": 28,
+				},
+				additionalKeys: []string{
+					"2002-02-03",
+					"2002-02-04",
+					"2002-02-05",
+					"2002-02-06",
+					"2002-02-07",
+					"2002-02-08",
+					"2002-02-09",
+					"2002-02-10",
+					"2002-02-11",
+					"2002-02-12",
+					"2002-02-13",
+					"2002-02-14",
+					"2002-02-15",
+					"2002-02-16",
+					"2002-02-17",
+					"2002-02-18",
+					"2002-02-19",
+					"2002-02-20",
+					"2002-02-21",
+					"2002-02-22",
+					"2002-02-23",
+					"2002-02-24",
+					"2002-02-25",
+					"2002-02-26",
+					"2002-02-27",
+					"2002-02-28",
+				},
+			},
 		}
-		for k, v := range got {
-			if vw, ok := want[k]; !ok || vw != v {
-				t.Errorf("%s: Value mismatch: FillMean(%v) = %v, want %v",
-					label, test.series, got, want)
+		for label, test := range tests {
+			want := createExpectedTimeSeriesFillNA(test.series, test.additionalKeys, m)
+			got, err := FillNA(test.series, m)
+
+			if err != nil {
+				t.Log(label, err)
+			}
+			if len(got) != len(want) {
+				t.Errorf("%s: Size mismatch: Fill%v (%v) = %v, want %v",
+					label, m, test.series, got, want)
+			}
+
+			for k, v := range got {
+				if vw, ok := want[k]; !ok || vw != v {
+					t.Errorf("%s: Value mismatch: Fill%v (%v) = %v, want %v",
+						label, m, test.series, got, want)
+				}
+			}
+		}
+	}
+}
+
+// Test function for Interpolate; possible methods : {linear}
+// Values are produced using Python, Pandas linear interpolation method:
+func TestInterpolate(t *testing.T) {
+	methods := []string{"linear"}
+	for _, m := range methods {
+		tests := map[string]struct {
+			series         TimeSeries
+			additionalKeys map[string]InterpolateValues
+		}{
+			"empty": {
+				series:         TimeSeries{},
+				additionalKeys: map[string]InterpolateValues{},
+			},
+			"length 1": {
+				series:         TimeSeries{"2003": 42.5},
+				additionalKeys: map[string]InterpolateValues{},
+			},
+			"length 3, years": {
+				series: TimeSeries{
+					"2003": 42.5,
+					"2002": 18.9,
+					"1998": 28,
+					"1996": 25,
+				},
+				additionalKeys: map[string]InterpolateValues{
+					"1997": {linear: 26.500},
+					"1999": {linear: 25.725},
+					"2000": {linear: 23.450},
+					"2001": {linear: 21.175},
+				},
+			},
+			"length 3, 2-month gap": {
+				series: TimeSeries{
+					"2003-02": 42.5,
+					"2002-02": 18.9,
+					"2003-04": 28,
+				},
+				// TODO(eftekhari-mhs): edge case : 2002-01 fails currently.
+				additionalKeys: map[string]InterpolateValues{
+					"2002-04": {linear: 22.833},
+					"2002-06": {linear: 26.766},
+					"2002-08": {linear: 30.700},
+					"2002-10": {linear: 34.633},
+					"2002-12": {linear: 38.566},
+				},
+			},
+			"days": {
+				series: TimeSeries{
+					"2002-03-01": 42.5,
+					"2002-02-01": 18.9,
+					"2002-02-02": 28,
+				},
+				additionalKeys: map[string]InterpolateValues{
+					"2002-02-03": {linear: 28.537},
+					"2002-02-04": {linear: 29.074},
+					"2002-02-05": {linear: 29.611},
+					"2002-02-06": {linear: 30.148},
+					"2002-02-07": {linear: 30.685},
+					"2002-02-08": {linear: 31.222},
+					"2002-02-09": {linear: 31.759},
+					"2002-02-10": {linear: 32.296},
+					"2002-02-11": {linear: 32.833},
+					"2002-02-12": {linear: 33.370},
+					"2002-02-13": {linear: 33.907},
+					"2002-02-14": {linear: 34.444},
+					"2002-02-15": {linear: 34.981},
+					"2002-02-16": {linear: 35.518},
+					"2002-02-17": {linear: 36.055},
+					"2002-02-18": {linear: 36.592},
+					"2002-02-19": {linear: 37.129},
+					"2002-02-20": {linear: 37.666},
+					"2002-02-21": {linear: 38.203},
+					"2002-02-22": {linear: 38.740},
+					"2002-02-23": {linear: 39.277},
+					"2002-02-24": {linear: 39.814},
+					"2002-02-25": {linear: 40.351},
+					"2002-02-26": {linear: 40.888},
+					"2002-02-27": {linear: 41.425},
+					"2002-02-28": {linear: 41.962},
+				},
+			},
+		}
+		for label, test := range tests {
+			want := createExpectedTimeSeriesInterpolate(test.series, test.additionalKeys, m)
+			got, err := Interpolate(test.series, 1)
+
+			if err != nil {
+				t.Log(label, err)
+			}
+
+			if len(got) != len(want) {
+				t.Errorf("%s: Size mismatch: Fill%v (%v) = %v, want %v",
+					label, m, test.series, got, want)
+			}
+
+			for k, v := range got {
+				if vw, ok := want[k]; !ok || math.Abs(vw-v) > 0.001 {
+					t.Errorf("%s: Value mismatch: Fill%v (%v) = %v, want %v",
+						label, m, test.series, got, want)
+				}
 			}
 		}
 	}
