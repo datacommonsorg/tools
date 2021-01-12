b6d0b582ff4a70710f3812c8d909df63db0eabda
Prashanth Radhakrishnan <shanth@google.com>
2021-01-12 06:22:12 +0000

    Local build support.

---
+++ cloud_automation/gcf/README.md
@@ -0,0 +1,8 @@
+This directory contains Google Cloud Functions involved in BT cache generation.
+
+To validate that the cloud function builds, run:
+
+```
+cd cmd
+go run main.go
+```
---
+++ cloud_automation/gcf/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"context"
+	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+	"btcachegeneration"
+)
+
+func main() {
+	ctx := context.Background()
+	if err := funcframework.RegisterEventFunctionContext(ctx, "/", btcachegeneration.BTImportController); err != nil {
+		log.Fatalf("funcframework.RegisterEventFunctionContext: %v\n", err)
+	}
+	// Use PORT environment variable, or default to 8080.
+	port := "8080"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
+	if err := funcframework.Start(port); err != nil {
+		log.Fatalf("funcframework.Start: %v\n", err)
+	}
+}
