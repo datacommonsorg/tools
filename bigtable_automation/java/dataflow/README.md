888a28d8f7eeafba34661dca5e6360463fe061c5
Alex Chen <alexyfchen@google.com>
2022-12-15 13:51:43 -0800

    Introduce Dataflow Flex Templates for csv2bt pipeline

--- bigtable_automation/java/dataflow/README.md
+++ bigtable_automation/java/dataflow/README.md
@@ -7,16 +7,29 @@ The template is stored at [gs://datcom-templates/templates](https://pantheon.cor
 
 ## Build
 
-```
+### Compile
+
+```sh
 # In bigtable_automation/java/dataflow
 mvn compile
 ```
 
+### Uber-jar
+
+Uber-jars package the compiled classes and all dependency jars into a single jar under target.
+
+```sh
+# In bigtable_automation/java/dataflow
+mvn clean package
+```
+
 ## Validation
+
+### Classic Templates
 
 1.  Deploy change to the test template for validation.
 
-    ```
+    ```sh
     ./test.sh deploy
     ```
 
@@ -27,7 +40,7 @@ mvn compile
 
 3.  Run the Dataflow job in the test BT environment against the test template.
 
-    ```
+    ```sh
     ./test.sh run-cache branch_2021_01_08_13_25_48
     ```
 
@@ -43,27 +56,69 @@ mvn compile
 If you have a one-off CSV file that you want to load into the test environment,
 run the following command:
 
-  ```
-  ./test.sh run-csv <BT_TABLE_NAME> <GCS_CSV_FILE>
-  ```
+```sh
+
+./test.sh run-csv <BT_TABLE_NAME> <GCS_CSV_FILE>
+
+```
 
 For example:
 
-  ```
-  ./test.sh run-csv mixer_test_bt gs://datcom-store-test/mixer_test_cache.csv
-  ```
+```sh
+
+./test.sh run-csv mixer_test_bt gs://datcom-store-test/mixer_test_cache.csv
+
+```
 
 Where, `mixer_test_bt` is the BT table that will be created from
 `gs://datcom-store-test/mixer_test_cache.csv`.
+
+### Flex Templates
+
+[Flex Templates](https://cloud.google.com/dataflow/docs/guides/templates/using-flex-templates) are a new generation of Dataflow templates that packages dependencies in the form of a Docker image instead of Jars.
+
+The same pipeline code can generate both Classic and Flex templates. Flex templates can be validated the same way using the steps above by adding "-flex" to the subcommands like the following.
+
+```sh
+./test.sh deploy-flex
+./test.sh run-cache-flex branch_2021_01_08_13_25_48
+./test.sh run-csv-flex mixer_test_bt gs://datcom-store-test/mixer_test_cache.csv
+```
 
 ## Production Deployment
 
 After validating the change in test environment, deploy it to PROD template by
 running the following command.
 
-```
+### Deploying Classic Template
+
+```sh
+
 mvn compile exec:java -Dexec.mainClass=org.datacommons.dataflow.CsvImport -Dexec.args="--runner=DataflowRunner --project=datcom-store --stagingLocation=gs://datcom-templates/staging --templateLocation=gs://datcom-templates/templates/csv_to_bt --region=us-central1 --usePublicIps=false"
+
 ```
 
 NOTE: Running this may throw an exception, but as long as it says `BUILD
 SUCCESS` the template has been updated.
+
+### Deploying Flex Template
+
+NOTE: If the version in pom.xml changes, please update TAG field below as well.
+
+```sh
+
+TAG="0.0.2-SNAPSHOT"
+
+mvn clean package && gcloud dataflow flex-template build \
+    "gs://datcom-dataflow-templates/templates/flex/csv_to_bt.json" \
+     --image-gcr-path "gcr.io/datcom-ci/dataflow-templates/csv2bt:$TAG" \
+     --sdk-language "JAVA" \
+     --flex-template-base-image JAVA11 \
+     --metadata-file "csv2bt-template-metadata.json" \
+     --jar "target/dataflow-$TAG.jar" \
+     --project=datcom-store \
+     --env FLEX_TEMPLATE_JAVA_MAIN_CLASS="org.datacommons.dataflow.CsvImport"
+
+```
+
+The `image-gcr-path` field is the image to be built, does not have to exist.
--- bigtable_automation/java/dataflow/test.sh
+++ bigtable_automation/java/dataflow/test.sh
@@ -2,9 +2,36 @@
 
 set -x
 
+create_fresh_bt_table_for_testing () {
+  gcloud config set project google.com:datcom-store-dev
+  cbt -instance prophet-test deletetable $TABLE 2>/dev/null
+  cbt -instance prophet-test createtable $TABLE || { echo "createtable failed!"; exit 1; }
+  cbt -instance prophet-test createfamily $TABLE csv || { echo "createfamily failed!"; exit 1; }
+}
+
+
 if [[ "$1" == "deploy" ]]; then
 
   mvn compile exec:java -Dexec.mainClass=org.datacommons.dataflow.CsvImport -Dexec.args="--runner=DataflowRunner --project=google.com:datcom-store-dev --stagingLocation=gs://datcom-dataflow-templates/test_staging --templateLocation=gs://datcom-dataflow-templates/templates/csv_to_bt_test --region=us-central1 --usePublicIps=false"
+
+elif [[ "$1" == "deploy-flex" ]]; then
+
+  # Build the Uber-jar.
+  mvn clean package
+
+  # From pom.xml
+  TAG="0.0.2-SNAPSHOT"
+
+  # Submit a build job to create a Docker image for the flex template.
+  gcloud dataflow flex-template build \
+    "gs://datcom-dataflow-templates/templates/flex/csv_to_bt_test.json" \
+     --image-gcr-path "gcr.io/datcom-ci/dataflow-templates/csv2bt:$TAG" \
+     --sdk-language "JAVA" \
+     --flex-template-base-image JAVA11 \
+     --metadata-file "csv2bt-template-metadata.json" \
+     --jar "target/dataflow-$TAG.jar" \
+     --project=datcom-ci \
+     --env FLEX_TEMPLATE_JAVA_MAIN_CLASS="org.datacommons.dataflow.CsvImport"
 
 elif [[ "$1" == "run-cache" ]]; then
 
@@ -13,16 +40,37 @@ elif [[ "$1" == "run-cache" ]]; then
     exit 1
   fi
   TABLE=$2
+  JOB_SUFFIX="${TABLE//_/-}" # Job names cannot have underscores.
 
-  gcloud config set project google.com:datcom-store-dev
-  cbt -instance prophet-test deletetable $TABLE 2>/dev/null
-  cbt -instance prophet-test createtable $TABLE || { echo "createtable failed!"; exit 1; }
-  cbt -instance prophet-test createfamily $TABLE csv || { echo "createfamily failed!"; exit 1; }
+  create_fresh_bt_table_for_testing
 
-  gcloud dataflow jobs run test-csv2bt-${TABLE} \
+  gcloud dataflow jobs run test-csv2bt-${JOB_SUFFIX} \
     --gcs-location gs://datcom-dataflow-templates/templates/csv_to_bt_test \
     --region us-central1 \
     --parameters inputFile=gs://datcom-store/${TABLE}/cache.csv*,completionFile=gs://automation_control_test/finished-${TABLE}.txt,bigtableInstanceId=prophet-test,bigtableTableId=${TABLE},bigtableProjectId=google.com:datcom-store-dev
+
+elif [[ "$1" == "run-cache-flex" ]]; then
+
+  if [[ $# != 2 ]]; then
+    echo "Usage: $0 run-cache-flex <CACHE_NAME>" >&2
+    exit 1
+  fi
+  TABLE=$2
+
+  create_fresh_bt_table_for_testing
+
+  JOB_SUFFIX="${TABLE//_/-}" # Job names cannot have underscores.
+
+  gcloud dataflow flex-template run test-csv2bt-flex-${JOB_SUFFIX} \
+    --template-file-gcs-location \
+      "gs://datcom-dataflow-templates/templates/flex/csv_to_bt_test.json" \
+    --parameters inputFile="gs://datcom-store/${TABLE}/cache.csv*" \
+    --parameters completionFile="gs://automation_control_test/finished-${TABLE}.txt" \
+    --parameters bigtableInstanceId="prophet-test" \
+    --parameters bigtableTableId=${TABLE} \
+    --parameters bigtableProjectId=google.com:datcom-store-dev \
+    --parameters tempLocation=gs://datcom-store-dev-resources/tmp \
+    --region "us-central1"
 
 elif [[ "$1" == "run-csv" ]]; then
 
@@ -32,19 +80,46 @@ elif [[ "$1" == "run-csv" ]]; then
   fi
   TABLE=$2
   CSV_FILE=$3
+  JOB_SUFFIX="${TABLE//_/-}" # Job names cannot have underscores.
 
-  gcloud config set project google.com:datcom-store-dev
-  cbt -instance prophet-test deletetable $TABLE 2>/dev/null
-  cbt -instance prophet-test createtable $TABLE || { echo "createtable failed!"; exit 1; }
-  cbt -instance prophet-test createfamily $TABLE csv || { echo "createfamily failed!"; exit 1; }
+  create_fresh_bt_table_for_testing
 
-  gcloud dataflow jobs run test-csv2bt-${TABLE}-csv \
+  gcloud dataflow jobs run test-csv2bt-${JOB_SUFFIX}-csv \
     --gcs-location gs://datcom-dataflow-templates/templates/csv_to_bt_test \
     --parameters inputFile=${CSV_FILE},completionFile=gs://automation_control_test/finished-${TABLE}.txt,bigtableInstanceId=prophet-test,bigtableTableId=${TABLE},bigtableProjectId=google.com:datcom-store-dev
 
+elif [[ "$1" == "run-csv-flex" ]]; then
+
+  if [[ $# != 3 ]]; then
+    echo "Usage: $0 run-csv-flex <BT_TABLE_NAME> <GCS_CSV_FILE>" >&2
+    exit 1
+  fi
+  TABLE=$2
+  CSV_FILE=$3
+  JOB_SUFFIX="${TABLE//_/-}" # Job names cannot have underscores.
+
+  create_fresh_bt_table_for_testing
+
+  gcloud dataflow flex-template run test-csv2bt-flex-${JOB_SUFFIX}-csv \
+    --template-file-gcs-location \
+      "gs://datcom-dataflow-templates/templates/flex/csv_to_bt_test.json" \
+    --parameters inputFile="${CSV_FILE}" \
+    --parameters completionFile="gs://automation_control_test/finished-${TABLE}.txt" \
+    --parameters bigtableInstanceId="prophet-test" \
+    --parameters bigtableTableId=${TABLE} \
+    --parameters bigtableProjectId=google.com:datcom-store-dev \
+    --parameters tempLocation=gs://datcom-store-dev-resources/tmp \
+    --region "us-central1"
+
 else
+  echo "Classic Templates:" >&2
   echo "Usage: $0 deploy" >&2
   echo "Usage: $0 run-cache <CACHE_NAME>" >&2
   echo "Usage: $0 run-csv <BT_TABLE_NAME> <GCS_CSV_FILE>" >&2
+  echo "Flex Templates:" >&2
+  echo "Usage: $0 deploy-flex" >&2
+  echo "Usage: $0 run-cache-flex <CACHE_NAME>" >&2
+  echo "Usage: $0 run-csv-flex <BT_TABLE_NAME> <GCS_CSV_FILE>" >&2
   exit 1
 fi
+
