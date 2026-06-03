7d05ce14a5756b1385a6e129cfc8c93100dd156b
Paulo Ferreira Jorge <mfjpaulo@gmail.com>
2026-06-03 14:36:35 +0100

    Add Type Support

--- dataweaver/apps/web/src/app/layout.tsx
+++ dataweaver/apps/web/src/app/layout.tsx
@@ -3,6 +3,9 @@ import '~/styles/core.scss';
 import type { ReactNode } from 'react';
 import { MotionProvider } from '~/components/foundations/motion_provider';
 
+const FONT_URL =
+  'https://fonts.googleapis.com/css?family=Google+Sans:400,500,700&display=swap&lang=en';
+
 interface RootLayoutProps {
   children: ReactNode;
 }
@@ -10,6 +13,15 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com/"
+          crossOrigin="anonymous"
+        />
+        <link rel="preload" as="font" href={FONT_URL} crossOrigin="anonymous" />
+        <link href={FONT_URL} rel="stylesheet" />
+      </head>
       <body>
         <MotionProvider>
           <main>{children}</main>
