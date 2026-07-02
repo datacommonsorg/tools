1a490e0da30797cc0b411722b4de436d591169e6
Paulo <mfjpaulo@gmail.com>
2026-07-02 10:48:44 +0100

    Implement App Wide UI Improvements (#387)
    * Overhaul App Sizings
    
    * Prompt Tweaks
    
    * Scrollbar Tweaks
    
    * Update Scroll Across App
    
    * More
    
    * Misc.
    
    * Implement Dismissed Node Support
    
    * Add Fixed Outline Color
    
    * Focus Tweaks
    
    * Tweak Intro
    
    * Tweak Follow Up
    
    * Implement Animated Prompt Focus

--- dataweaver/apps/web/src/components/elements/tag.tsx
+++ dataweaver/apps/web/src/components/elements/tag.tsx
@@ -1,27 +1,13 @@
-import { IconClose } from '~/components/primitives/icons/close';
-import { Button } from './button';
 import s from './tag.module.scss';
 
 interface TagProps {
   label: string;
-  onRemove?: () => void;
 }
 
-export const Tag = ({ label, onRemove }: TagProps) => {
+export const Tag = ({ label }: TagProps) => {
   return (
-    <span className={s.container} data-has-icon={onRemove !== undefined}>
+    <span className={s.container}>
       <span className={s.label}>{label}</span>
-
-      {onRemove && (
-        <Button
-          size="extra-small"
-          variant="flat"
-          tone="subtle"
-          icon={IconClose}
-          aria-label={`Remove ${label}`}
-          onClick={onRemove}
-        />
-      )}
     </span>
   );
 };
